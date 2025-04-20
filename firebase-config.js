e68dc3e9084356d59152bc1a10e98a8cb79a3f95
radstad12 <radstad12@seznam.cz>
2025-04-20 04:17:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,16 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  onValue,
-  set,
-  remove,
-  push
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
-// Konfigurace z tvého Firebase projektu
 const firebaseConfig = {
   apiKey: "AIzaSyBLPUkWM8j77f1rs8ZrVUAPX2AmDU9Ocx4",
   authDomain: "cartarena-ls.firebaseapp.com",
-  databaseURL: "https://cartarena-ls-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "cartarena-ls",
-  storageBucket: "cartarena-ls.appspot.com",
+  storageBucket: "cartarena-ls.firebasestorage.app",
   messagingSenderId: "485144557401",
   appId: "1:485144557401:web:077474913768f62e70fe48",
-  measurementId: "G-YXJ0XRGBTL"
+  measurementId: "G-YXJ0XRGBTL",
+  databaseURL: "https://cartarena-ls-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
-// Inicializace Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Export potřebných funkcí
-export { db, ref, onValue, set, remove, push };
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
