6c92c904f661ae74372c9899a019a2b004e33b3d
radstad12 <radstad12@seznam.cz>
2025-04-20 03:57:14 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,12 @@
-// firebase-config.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
-import { getDatabase, ref, onValue, set, remove } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js';
-
 const firebaseConfig = {
   apiKey: "AIzaSyBLPUkWM8j77f1rs8ZrVUAPX2AmDU9Ocx4",
   authDomain: "cartarena-ls.firebaseapp.com",
-  databaseURL: "https://cartarena-ls-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://cartarena-ls-default-rtdb.europe-west1.firebasedatabase.app", // ← SEM!
   projectId: "cartarena-ls",
-  storageBucket: "cartarena-ls.firebasestorage.app",
+  storageBucket: "cartarena-ls.appspot.com",
   messagingSenderId: "485144557401",
-  appId: "1:485144557401:web:077474913768f62e70fe48"
+  appId: "1:485144557401:web:077474913768f62e70fe48",
+  measurementId: "G-YXJ0XRGBTL"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, onValue, set, remove };
+firebase.initializeApp(firebaseConfig);
